e571db2294ccf6d5bb85afdca3e652c8a7e4a79b
Felix-Chaos <felixx511007@gmail.com>
2026-06-05 21:52:28 +0200

    Fix version in updater.json to properly flag beta release

--- test_manifest.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src-tauri/assets/resources/manifest.json', 'utf8');
-const val = JSON.parse(content);
-const pack_ver = "1.10.1";
-const patch_ver = "1.2";
-const formatted_name = `§eActions & Stuff §dRTX §b${pack_ver.replace('v', '')} §5V${patch_ver}`;
-val.header.name = formatted_name;
-console.log(val.header.name);
